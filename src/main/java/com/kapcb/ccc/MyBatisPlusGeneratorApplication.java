d08c69cadfd767866f9475489afa808c8dbdba7b
kapcb <2530628867@qq.com>
2022-10-24 21:33:23 +0800

    âinitial

--- src/main/java/com/kapcb/ccc/MyBatisPlusGeneratorApplication.java
+++ src/main/java/com/kapcb/ccc/MyBatisPlusGeneratorApplication.java
@@ -2,6 +2,7 @@ package com.kapcb.ccc;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
@@ -14,11 +15,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @date 2022/7/18 17:57
  * @since 1.0
  */
-@SpringBootApplication
-public class MyBatisGeneratorApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MyBatisPlusGeneratorApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MyBatisGeneratorApplication.class)
+        new SpringApplicationBuilder(MyBatisPlusGeneratorApplication.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
